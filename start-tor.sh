c01edc2652c356a91300f417ab4c54364e47a56a
Philipp Winter <phw@torproject.org>
2020-02-04 16:11:41 -0800

    Update and improve comments.

--- start-tor.sh
+++ start-tor.sh
@@ -7,8 +7,8 @@ RunAsDaemon 0
 # We don't need an open SOCKS port.
 SocksPort 0
 BridgeRelay 1
-# A static nickname makes it easy to identify bridges that were set up in this
-# Docker container.
+# A static nickname makes it easy to identify bridges that were set up using
+# this Docker image.
 Nickname DockerObfs4Bridge
 Log notice file /var/log/tor/log
 Log notice stdout
@@ -16,16 +16,13 @@ ServerTransportPlugin obfs4 exec /usr/bin/obfs4proxy
 ExtORPort auto
 DataDirectory /var/lib/tor
 
-# The variable "OR_PORT" is replaced with a randomly-generated OR port by the
-# script deploy-container.sh.
+# The variable "OR_PORT" is replaced with the OR port.
 ORPort ${OR_PORT}
 
-# The variable "PT_PORT" is replaced with a randomly-generated port by the
-# script deploy-container.sh.
+# The variable "PT_PORT" is replaced with the obfs4 port.
 ServerTransportListenAddr obfs4 0.0.0.0:${PT_PORT}
 
-# The variable "EMAIL" is replaced with the operator's email address by the
-# script deploy-container.sh.
+# The variable "EMAIL" is replaced with the operator's email address.
 ContactInfo ${EMAIL}
 EOF
 
