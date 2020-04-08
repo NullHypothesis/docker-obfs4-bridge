d7739b71240ef0eadb5f079932ccd8cab86fe478
ThinkChaos <ThinkChaos@users.noreply.github.com>
2020-04-08 22:33:28 +0200

    Make torrc static and include runtime-generated config files

--- Dockerfile
+++ Dockerfile
@@ -28,10 +28,14 @@ RUN apt-get update && apt-get install -y \
 # Allow obfs4proxy to bind to ports < 1024.
 RUN setcap cap_net_bind_service=+ep /usr/bin/obfs4proxy
 
-# Our torrc is generated at run-time by the script start-tor.sh.
-RUN rm /etc/tor/torrc
 RUN chown debian-tor:debian-tor /etc/tor
 RUN chown debian-tor:debian-tor /var/log/tor
+
+RUN mkdir /etc/tor/torrc.d
+RUN chown debian-tor:debian-tor /etc/tor/torrc.d
+
+COPY torrc /etc/tor
+RUN chown debian-tor:debian-tor /etc/tor/torrc
 
 COPY start-tor.sh /usr/local/bin
 RUN chmod 0755 /usr/local/bin/start-tor.sh
--- start-tor.sh
+++ start-tor.sh
@@ -2,27 +2,10 @@
 
 echo "Using OR_PORT=${OR_PORT}, PT_PORT=${PT_PORT}, and EMAIL=${EMAIL}."
 
-cat > /etc/tor/torrc << EOF
-RunAsDaemon 0
-# We don't need an open SOCKS port.
-SocksPort 0
-BridgeRelay 1
-# A static nickname makes it easy to identify bridges that were set up using
-# this Docker image.
-Nickname DockerObfs4Bridge
-Log notice file /var/log/tor/log
-Log notice stdout
-ServerTransportPlugin obfs4 exec /usr/bin/obfs4proxy
-ExtORPort auto
-DataDirectory /var/lib/tor
-
-# The variable "OR_PORT" is replaced with the OR port.
+# Add configuration from docker-compose's environment
+cat > /etc/tor/torrc.d/env << EOF
 ORPort ${OR_PORT}
-
-# The variable "PT_PORT" is replaced with the obfs4 port.
 ServerTransportListenAddr obfs4 0.0.0.0:${PT_PORT}
-
-# The variable "EMAIL" is replaced with the operator's email address.
 ContactInfo ${EMAIL}
 EOF
 
